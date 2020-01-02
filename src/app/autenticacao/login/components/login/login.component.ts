f5ccd4a157a3262be74ed27c99ce091bc97c3680
danspiegel <danispiegel@gmail.com>
2020-01-02 01:04:10 -0300

    Criação da tela de login e chamada da API java para login do sistema de ponto inteligente utilizando Angular

---
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+
+import { Login } from '../../models';
+
+import { LoginService } from '../../services';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private loginService: LoginService
+  ) { }
+
+  ngOnInit() {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  logar() {
+    if (this.form.invalid) {
+      this.snackBar.open(
+        "Dados inválidos", "Erro", {duration: 5000}
+      );
+      return;
+    }
+    const login: Login = this.form.value;
+    this.loginService.logar(login)
+      .subscribe(
+        data => {
+          console.log(JSON.stringify(data));
+          localStorage['token'] = data['data']['token'];
+          const usuarioData = JSON.parse(atob(data['data']['token'].split('.')[1]));
+          console.log(JSON.stringify(usuarioData));
+          if (usuarioData['role'] == 'ROLE_ADMIN') {
+            alert('Deve redirecionar para a página de admin.');
+          } else {
+            alert('Deve redirecionar para a página de funcionário.');
+          }
+        },
+        err => {
+          console.log(JSON.stringify(err));
+          let msg: string = "Tente novamente em instantes.";
+          if (err['status'] == 401) {
+            msg = "Email/Senha inválidos.";
+          }
+          this.snackBar.open(msg, "Erro", {duration : 5000});
+        }
+      );
+  }
+
+}
