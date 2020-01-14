1c77791036237f3d6ab1340b727a4fb62cfe7d90
danspiegel <danispiegel@gmail.com>
2020-01-14 18:02:08 -0300

    Criado cadastro de pessoa física.

---
+++ src/app/autenticacao/cadastro-pf/models/cadastro-pf.model.ts
@@ -0,0 +1,13 @@
+export class CadastroPf {
+
+    constructor(
+        private nome: string,
+        private email: string,
+        private senha: string,
+        private cpf: string,
+        private cnpj: string
+    ) {
+
+    }
+
+}
